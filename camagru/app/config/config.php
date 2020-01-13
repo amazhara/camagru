86642f340a3b03c97db8daa4dc4ce8778bd44524
amazhara <levilater@gmail.com>
2020-01-13 14:51:17 +0200

    Add project and database config files

---
+++ camagru/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+//App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+//URL Root
+define('URLROOT', 'http://192.168.99.100:8080/camagru');
+//Site Name
+define('SITENAME', 'Camagru');
