b479ceba7ceda3eb88fb853ddd160a77db0e23d1
amazhara <levilater@gmail.com>
2020-01-14 18:15:32 +0200

    Add additional bootstrap toolkit dependencies

---
+++ camagru/app/view/include/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap css settings -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" integrity="sha384-REHJTs1r2ErKBuJB0fCK99gCYsVjwxHrSU0N7I1zl9vZbggVJXRMsv/sLlOAGb4M" crossorigin="anonymous">
+
+    <!-- My css settings -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
+    <title><?php echo SITENAME; ?></title>
+</head>
+<body>
+<?php require_once APPROOT . '/view/include/navbar.php' ?>
+<div class="container">
