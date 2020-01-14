e21817a1bbd92d21f7b242d95091fecb07dfabcb
amazhara <levilater@gmail.com>
2020-01-14 18:08:24 +0200

    Add debug option to index.php

--- camagru/public/index.php
+++ camagru/public/index.php
@@ -1,8 +1,10 @@
 <?php
 
+// For debug purpose
+//header('Content-Type:text/plain');
+
 // TODO move error reporting setup to another place
 // Turn on error messages
-
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -11,3 +13,19 @@ include_once "../app/bootstrap.php";
 
 // Init app
 $init = new Router;
+
+
+
+
+
+
+
+
+
+
+// TODO make normal frontend views with separate header and footer
+// TODO include bootstrap in frontend
+// TODO add User controller model and view functional
+// TODO User registration
+// TODO User login
+// TODO User change password
