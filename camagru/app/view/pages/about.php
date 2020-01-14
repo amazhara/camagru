13e72ca22f458c41a4a82d7435fe0cc74109f241
amazhara <levilater@gmail.com>
2020-01-14 18:12:39 +0200

    Add view for pages about method

---
+++ camagru/app/view/pages/about.php
@@ -0,0 +1,7 @@
+<?php require APPROOT . '/view/include/header.php'; ?>
+<div class="container">
+    <h1><?php echo $data['title']; ?></h1>
+    <p ><?php echo $data['description']; ?></p>
+    <p ><?php echo $data['author']; ?></p>
+</div>
+<?php require APPROOT . '/view/include/footer.php'; ?>
