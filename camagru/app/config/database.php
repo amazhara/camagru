09a9d7d0622d48aeb0b79c29191af89598d408ad
amazhara <levilater@gmail.com>
2020-01-14 11:14:22 +0200

    Update database config file

--- camagru/app/config/database.php
+++ camagru/app/config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'http://192.168.99.100:3306');
+define('DB_HOST', '192.168.99.100');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'camagru');
