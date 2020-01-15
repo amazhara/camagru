ffaab3edf2c58dde9ec68f3b0d8c50d5ab32e8cb
amazhara <levilater@gmail.com>
2020-01-15 18:46:56 +0200

    Update navigation bar view functional

--- camagru/app/view/include/navbar.php
+++ camagru/app/view/include/navbar.php
@@ -5,9 +5,8 @@
                 aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-    </div>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
@@ -16,5 +15,25 @@
                     <a class="nav-link" href="<?php echo URLROOT; ?>pages/about">About</a>
                 </li>
             </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <?php if (isset($_SESSION['user_id'])) : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);">Welcome <?php echo $_SESSION['user_name']; ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>users/logout">Logout</a>
+                    </li>
+                <?php else : ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>users/register">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT; ?>users/login">Login</a>
+                    </li>
+                <?php endif; ?>
+
+            </ul>
         </div>
+    </div>
 </nav>
