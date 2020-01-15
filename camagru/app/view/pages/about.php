287bad5d6f5a8ae0dfd32bf88de2305b99f4e83d
amazhara <levilater@gmail.com>
2020-01-15 10:08:34 +0200

    Update Pages controller view

--- camagru/app/view/pages/about.php
+++ camagru/app/view/pages/about.php
@@ -1,7 +1,11 @@
 <?php require APPROOT . '/view/include/header.php'; ?>
-<div class="container">
-    <h1><?php echo $data['title']; ?></h1>
-    <p ><?php echo $data['description']; ?></p>
-    <p ><?php echo $data['author']; ?></p>
+<div class="alert alert-info text-center" role="alert">
+    <h1 class="font-weight-normal"><?php echo $data['title']; ?></h1>
+    <p class="text-light"><?php echo $data['description']; ?></p>
+</div>
+<hr>
+<div class="container text-center">
+    <a class="btn btn-sm btn-success" href="https://github.com/amazhara"
+       target="_blank"><?php echo $data['author']; ?></a>
 </div>
 <?php require APPROOT . '/view/include/footer.php'; ?>
