bbe7364472f4ef69006c8a5f832f3ce19c171020
amazhara <levilater@gmail.com>
2020-01-18 18:57:07 +0200

    Fix session helper

--- camagru/app/helpers/session_helper.php
+++ camagru/app/helpers/session_helper.php
@@ -1,10 +1,10 @@
 <?php
 
 // Start session every time program instantiates
-session_status();
+session_start();
 
 // Default check if user is logged in
-function isLoggedIn() : bool {
+function isLoggedIn(): bool {
     if (isset($_SESSION['user_id'])) {
         return true;
     } else {
@@ -12,13 +12,15 @@ function isLoggedIn() : bool {
     }
 }
 
-
-// Show flesh message to users
+/*
+ * Show flesh message to users
+ * Message is held in $_SESSION array
+ * First call to save massage -> flash('register_success', 'You can now log in')
+ * Then call with only name to print -> flash('register_success')
+ */
 function flash($name = '', $message = '', $class = 'alert alert-success') {
     if (!empty($name)) {
-
-        if (!empty($message) && empty($_SESSION['name'])) {
-
+        if (!empty($message) && empty($_SESSION[$name])) {
             if (!empty($_SESSION[$name])) {
                 unset($_SESSION[$name]);
             }
@@ -28,15 +30,14 @@ function flash($name = '', $message = '', $class = 'alert alert-success') {
             }
 
             $_SESSION[$name] = $message;
-            $_SESSION[$name. '_class'] = $class;
-
-        } elseif (empty($message) && !empty($_SESSION['name'])) {
+            $_SESSION[$name . '_class'] = $class;
+        } elseif (empty($message) && !empty($_SESSION[$name])) {
 
             $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
             echo '<div class="' . $class . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
-
             unset($_SESSION[$name]);
-            unset($_SESSION[$name. '_class']);
+            unset($_SESSION[$name . '_class']);
         }
     }
 }
+
