a801a204382e4ff9e9d74bb49549a858ebe6f9b1
amazhara <levilater@gmail.com>
2020-01-23 17:04:26 +0200

    Add snap photo controller and view

--- camagru/app/controllers/Posts.php
+++ camagru/app/controllers/Posts.php
@@ -12,16 +12,39 @@ class Posts extends Controller {
 
     private $userModel;
 
-    function __construct() {
+    public function __construct() {
         $this->userModel = $this->model('User');
     }
 
-    function add($data = []) {
+    public function snap() {
+        // To catch fetch request
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//            $image = file_get_contents('php://input');
+//            $str = json_decode($str);
+//            $str = json_encode($str);
+//            var_dump($str);
+//            echo $image;
+//            echo false;
+//            $_POST['image'] = $image;
+//            $this->add('asfdk;sadfl;saj');
+//            var_dump($_FILES);
+//                    var_dump($_SERVER);
+//            $l = file_get_contents('php://input');
+//            var_dump($l);
+//            die('lOOOOL');
+        } else {
+            // Call view
+            $this->view('posts/snap');
+        }
+    }
+
+    public function add() {
+//        var_dump($_POST);
+//        var_dump($_FILES);
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             var_dump($_POST);
-            echo '<br>';
-            var_dump($_FILES);
+        } else {
+            die('l');
         }
-        $this->view('posts/add');
     }
 }
