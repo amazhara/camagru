e2e8b2336a81689bf9cbc17d081cd261816d410f
amazhara <levilater@gmail.com>
2020-01-26 16:11:45 +0200

    Update post model with additional functions

--- camagru/app/models/Post.php
+++ camagru/app/models/Post.php
@@ -13,11 +13,26 @@ class Post
 {
     private $db;
 
-    function __construct() {
+    public function __construct() {
         $this->db = new Database;
     }
 
-    function add($data) {
+    public function getPosts() {
+        $this->db->query(
+            "SELECT *,
+                 posts.id as postId,
+                 users.id as userId,
+                 posts.created_at as postCreated,
+                 users.created_at as userCreated
+                 FROM posts
+                 INNER JOIN users
+                 ON posts.user_id = users.id
+                 ORDER BY posts.created_at DESC 
+            ");
+        return $this->db->resultSet();
+    }
+
+    public function add($data) : bool {
         // Set prepared query
         $this->db->query('INSERT INTO posts (photo, user_id, body) VALUES (:photo, :user_id, :body)');
         // Bind values
@@ -32,7 +47,22 @@ class Post
         }
     }
 
-//    function findPostById($id) {
-//
-//    }
+    public function findPostById($id) {
+        $this->db->query('SELECT * FROM posts where id = :id');
+        $this->db->bind(':id', $id);
+
+        $post = $this->db->single();
+        return $post;
+    }
+
+    public function deletePostById($id) : bool {
+        $this->db->query('DELETE * FROM posts where id = :id');
+        $this->db->bind(':id', $id);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
