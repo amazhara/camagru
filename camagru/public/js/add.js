393479923db7679868840fcfeed69119684775f4
amazhara <levilater@gmail.com>
2020-02-02 15:46:39 +0200

    Fix bug when photos from history cannot be saved. Move JS to public part of project

--- camagru/public/js/add.js
+++ camagru/public/js/add.js
@@ -1,4 +1,4 @@
-let width = 640;  // We will scale the photo width to this
+let width = 640;  // Scale the photo width to this
 let height = 0;   // This will be computed based on the input stream
 
 // indicates whether or not we're currently streaming
@@ -8,6 +8,11 @@ let streaming = false;
 
 let video = document.getElementById('video');
 let canvas = document.getElementById('canvas');
+let image = document.getElementById('image');
+let picture = document.getElementById('pic');
+let paste = document.getElementById('paste');
+let color = '';
+
 
 // Get access to the camera!
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -35,39 +40,88 @@ video.addEventListener('canplay', function () {
     }
 });
 
-document.getElementById("snap").addEventListener('click', function () {
-    takePicture();
+document.getElementById('place').addEventListener('click', function () {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image.src);
+    newImage.setAttribute('class', 'rounded float-left mt-2 ml-2');
+    newImage.setAttribute('width', '200px');
+    newImage.setAttribute('height', '200px');
+    newImage.setAttribute('data-toggle', 'modal');
+    newImage.setAttribute('data-target', '#Modal');
+    newImage.onclick = function () {
+        image.src = newImage.src;
+    };
+    console.log(newImage);
+    paste.appendChild(newImage);
 });
 
-// Fetch (ajax) image to php
-// document.getElementById("snap").addEventListener('click', async function () {
-//     takePicture();
-//     // Create blob instance to send photo
-//     let blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
-//
-//     // Send photo
-//     let response = await fetch('http://localhost:8080/posts/add', {
-//         method: 'POST',
-//         body: blob
-//     });
-//
-//     // Wait for response from php
-//     let result = await response.text();
-//
-//     // window.location = "http://localhost:8080/posts/add";
-//     // Convert blob to png
-//     // result = result.slice(0, blob.size, 'image/png');
-//
-//     console.log(result);
-//     // link.href = URL.createObjectURL(result);
-//     // console.log(link);
-// });
+picture.addEventListener('change', function () {
+    image.src = URL.createObjectURL(picture.files[0]);
+});
+
+document.getElementById("snap").addEventListener('click', function () {
+    takePicture();
+    image.setAttribute('src', canvas.toDataURL());
+});
+
+document.getElementById("sepia").addEventListener('click', function () {
+    color = 'sepia(1)';
+});
+document.getElementById("invert").addEventListener('click', function () {
+    color = 'invert(50)';
+});
+document.getElementById("blur").addEventListener('click', function () {
+    color = 'blur(5px)';
+});
+document.getElementById("strongblur").addEventListener('click', function () {
+    color = 'blur(20px)';
+});
+document.getElementById("default").addEventListener('click', function () {
+    color = '';
+});
+
+window.setInterval(function() {
+    takePicture();
+}, 8);
+
+document.getElementById('send').addEventListener('click', function () {
+    let body = document.getElementById('body');
+
+    // Check if user entered comment
+    if (body.value === 'Your comment') {
+        alert('Please enter your comment!');
+        return;
+    }
+
+    // Create form to send
+    let form = new FormData;
+
+    canvas.toBlob(function (blob) {
+        // Add photo to form
+        form.append('photo', blob, 'photo.png');
+        // Add body to form
+        form.append('body', body.value);
+        // console.log(form.get('body'));
+        // console.log(form.get('photo'));
+
+        let response = fetch('<?php echo URLROOT; ?>/posts/add', {
+            method: 'POST',
+            body: form
+        }).then(response => response.text())
+        // .then(response => console.log(response))
+            .then(response => window.location.replace('<?php echo URLROOT; ?>/posts'))
+
+    }, 'image/png');
+});
 
 function takePicture() {
     const context = canvas.getContext('2d');
     if (width && height) {
+
         canvas.width = width;
         canvas.height = height;
+        context.filter = color;
+        context.fillRect(0,0,width,height);
         context.drawImage(video, 0, 0, width, height);
     }
 }
